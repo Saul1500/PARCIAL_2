30b99cb7a060ddfd848f550a68b2290168aed5b7
Saul1500 <125324281+Saul1500@users.noreply.github.com>
2023-04-18 20:10:58 -0600

    Add files via upload

---
+++ PARCIAL 2/PARCIAL 2/Program.cs
@@ -0,0 +1,16 @@
+﻿int dato1 = 16;
+string dato2 = "25g";
+int resultado;
+
+try
+{
+    resultado = dato1 + Convert.ToInt32(dato2);
+    Console.WriteLine("El resultado es: " + resultado);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
+
+
+// ALUMNO: JOSE SAUL SIBRIAN SERRANO
